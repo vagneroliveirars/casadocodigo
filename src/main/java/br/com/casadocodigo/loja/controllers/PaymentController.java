801cf0101d2f9374f05040dfaac4565c1f975b4f
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-11-23 19:43:00 -0200

    Asynchronous integration with the external payment system.

--- src/main/java/br/com/casadocodigo/loja/controllers/PaymentController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PaymentController.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.loja.controllers;
 
 import java.math.BigDecimal;
+import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.models.PaymentData;
 import br.com.casadocodigo.loja.models.ShoppingCart;
@@ -30,23 +32,26 @@ public class PaymentController {
 	private RestTemplate restTemplate;
 
 	/**
-	 * Performs integration with the external payment system
+	 * Performs an asynchronous integration with the external payment system
 	 * 
 	 * @return redirection URL
 	 */
 	@RequestMapping(value = "checkout", method = RequestMethod.POST)
-	public String checkout() {
-		BigDecimal total = shoppingCart.getTotal();
-		
-		String uriToPay = "http://book-payment.herokuapp.com/payment";
-		
-		try {
-			String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
-			System.out.println(response);
-			return "redirect:/payment/success";
-		} catch (HttpClientErrorException e) {
-			return "redirect:/payment/error";
-		}
+	public Callable<ModelAndView> checkout() {
+		return new Callable<ModelAndView>() {
+			@Override
+			public ModelAndView call() throws Exception {
+				BigDecimal total = shoppingCart.getTotal();
+				String uriToPay = "http://book-payment.herokuapp.com/payment";
+				
+				try {
+					String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
+					return new ModelAndView("redirect:/payment/success");
+				} catch (HttpClientErrorException e) {
+					return new ModelAndView("redirect:/payment/error");
+				}
+			}
+		};
 	}
 
 }
